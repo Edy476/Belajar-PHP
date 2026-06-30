8bf530132e62546381740b381ea39ed076554f2c
Edy476 <loclactt@gmail.com>
2026-06-30 19:49:49 +0800

    Day 3 - Belajar PHP - Penambahan isi File (masih sibuk nugas jadi tidak bisa belajar banget)

--- array/array_multidimensi.php
+++ array/array_multidimensi.php
@@ -1,20 +1,32 @@
 <?php
-    $mahasiswa = ["Edy Sukarma", "03040555" ,"Teknik Informatika", "edy067@gmail.com"];
+    $mahasiswa2 = [
+        ["Edy Sukarma", "03040555" ,"Teknik Informatika", "edy067@gmail.com"],
+        ["Ahmad Kurniawan", "03040675" ,"Teknik Informatika", "Kurni067@gmail.com"]
+    ];
+    $mahasiswa = ["amad", "123456", "Teknik Listrik","amad123@gmail.com"];
 
 ?>
-
-<!DOCTYPE html>
+<html>
 <head>
     <title>Daftar Data Mahasiswa</title>
 </head>
 <body>
-    <h1>Daftar Data Mahasiswa</h1>
+    <h1>Daftar Data Mahasiswa dengan array multidimensi</h1>
+    <?php foreach($mahasiswa2 as $mhs) : ?>
     <ul>
-        <li><?= $mahasiswa[0]; ?></li>
-        <li><?= $mahasiswa[0]; ?></li>
-        <li><?= $mahasiswa[0]; ?></li>
-        <li><?= $mahasiswa[0]; ?></li>
-        <li><?= $mahasiswa[0]; ?></li>
+           <li> Nama: <?= $mhs[0]; ?>  </li>
+           <li> Nim: <?= $mhs[1]; ?>  </li>
+           <li> Email: <?= $mhs[3]; ?>  </li>
+           <li> Prodi: <?= $mhs[2]; ?>  </li>
+    </ul>
+    <?php endforeach; ?>
+
+    <h1>daftar mahasiswa array biasa</h1>
+    <ul>
+        <li><?= $mahasiswa[0]; ?></li> 
+        <li><?= $mahasiswa[1]; ?></li> 
+        <li><?= $mahasiswa[2]; ?></li> 
+        <li><?= $mahasiswa[3]; ?></li> 
     </ul>
 </body>
 </html>
